326e402661b3d536fe120cb681301bb792831a3e
jundonghyun <70741953+jundonghyun@users.noreply.github.com>
2021-08-03 12:42:06 +0900

    등굣길 DP로 풀기

---
+++ algorithm/Programmers/JAVA/LV3/School_Route.java
@@ -0,0 +1,47 @@
+package Programmers.JAVA.LV3;
+
+public class School_Route {
+    public static void main(String[] args){
+
+        int m,n;
+        m = 4;
+        n = 3;
+
+        int[][] puddles = {{2,2}};
+
+        solution(m, n, puddles);
+
+
+    }
+
+    public static int solution(int m, int n, int[][] puddles) {
+        int answer = 0;
+        int[][] arr = new int[n][m];
+
+        
+        arr[0][0] = 1;
+
+        for(int i = 0; i < puddles.length; i++){
+            arr[puddles[i][1]-1][puddles[i][0]-1] = 9;
+        }
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(arr[i][j] == 9){
+                    arr[i][j] = 0;
+                    continue;
+                }
+                else{
+                    if(i != 0){
+                        arr[i][j] += arr[i-1][j] % 1000000007;
+                    }
+                    if(j != 0){
+                        arr[i][j] += arr[i][j-1] % 1000000007;
+                    }
+                }
+            }
+        }
+
+        return arr[n-1][m-1] % 1000000007;
+    }
+}
