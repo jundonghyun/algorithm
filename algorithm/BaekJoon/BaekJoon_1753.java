a2cffa9ba4b3bc2d0c83fc39035b969d4eeb938a
jundonghyun <70741953+jundonghyun@users.noreply.github.com>
2021-08-24 16:27:29 +0900

    최단경로 코드 간결화

--- algorithm/BaekJoon/BaekJoon_1753.java
+++ algorithm/BaekJoon/BaekJoon_1753.java
@@ -1,10 +1,7 @@
 package BaekJoon;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 
 public class BaekJoon_1753 {
