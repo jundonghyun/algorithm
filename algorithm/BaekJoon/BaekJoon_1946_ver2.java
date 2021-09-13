bcd7db9c74acc2af10ecf0d143d89a8862687109
jundonghyun <70741953+jundonghyun@users.noreply.github.com>
2021-09-13 17:12:19 +0900

    신입사원 자바풀이 정답
    복잡하게 모두 비교할필요없이 면접등수로만 비교하면 됬었음

---
+++ algorithm/BaekJoon/BaekJoon_1946_ver2.java
@@ -0,0 +1,71 @@
+package BaekJoon;
+
+import java.io.*;
+import java.util.*;
+
+public class BacekJoon_1946_ver2 {
+    static int[] result;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int T = Integer.parseInt(st.nextToken());
+
+        List<Employee> list[] = new ArrayList[T];
+        List<Integer> temp[] = new ArrayList[T];
+        result = new int[T];
+
+        for(int i = 0; i < T; i++){
+            list[i] = new ArrayList<>();
+            temp[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < T; i++){
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+
+            for(int j = 0; j < n; j++){
+                st = new StringTokenizer(br.readLine());
+
+                int p = Integer.parseInt(st.nextToken());
+                int inter = Integer.parseInt(st.nextToken());
+
+                list[i].add(new Employee(p, inter));
+            }
+            Collections.sort(list[i]); //서류등수로 오름차순 정렬
+        }
+
+        for(int i = 0; i < T; i++){
+            int count = 1;//서류 1등은 항상 존재하므로 1부터 카운트 시작
+            int idx = 0; //면접등수가 높은 사람을 찾을 변수
+
+            for(int j = 1; j < list[i].size(); j++){
+                if(list[i].get(j).interview < list[i].get(idx).interview){
+                    // 1. 제일 초기상황 : 1등면접점수보다 현재 면접점수가 높을경우
+                    // 2. 이후 상황 : 이전의 면접점수보다 현재 면접점수가 높을경우
+                    count++;
+                    idx = j; // 현재 면접등수의 위치를 저장(리스트로 저장되어있으므로)
+                    continue;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+
+    
+}
+
+class Employee implements Comparable<Employee>{
+    int paper;
+    int interview;
+
+    Employee(int p, int i){
+        this.paper = p;
+        this.interview = i;
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        return paper - o.paper;
+    }
+}
