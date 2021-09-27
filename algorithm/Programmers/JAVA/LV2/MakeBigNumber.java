cdf1ee0ab2f043c6e196d5421c67d15c5b7165f3
jundonghyun <70741953+jundonghyun@users.noreply.github.com>
2021-09-27 17:37:35 +0900

    Update MakeBigNumber.java

--- algorithm/Programmers/JAVA/LV2/MakeBigNumber.java
+++ algorithm/Programmers/JAVA/LV2/MakeBigNumber.java
@@ -14,27 +14,28 @@ public class MakeBigNumber {
 
         solution(number ,k);
     }
-    //t배열을 가장뒤에부터 
+
     public static String solution(String number, int k) {
         StringBuilder sb = new StringBuilder();
 	    
-        int left = 0;
-        int right = k;
-        int max = -1;
-        int idx = 0;
+        int left = 0; //시작위치
+        int right = k; //종료위치
+        int idx = 0; //가장큰 값의 인덱스를 저장
         
         while(sb.length() < number.length() - k) {
-        	 max = -1;
-             for(int j = left ; j <= right ; ++j){
-                 int num = number.charAt(j) - '0';
-                 if(num > max){
-                     idx = j;
-                     max = num;
+            int max = -1; //left와 right구간내에서 가장큰 값을 저장
+            for(int j = left ; j <= right ; ++j){
+                 int num = number.charAt(j) - '0'; //left부터 시작해서 right까지 값을 가져옴
+                 if(num > max){ //가져온 값이 max보다 크다면
+                     idx = j; //그 위치를 저장
+                     max = num; //max는 큰값으로 변경
                  }
              }
-             sb.append(number.charAt(idx));
-             left = idx + 1;
-             right += 1;
+             sb.append(max); //sb에 max값을 추가
+             left = idx + 1; //idx앞으로는 탐색할 필요없음 -> max보다 작은수이기때문
+             //그래서 시작위치는 left는 idx+1로 변경
+             right += 1; //right는 그냥+1해도 어차피 idx는 left와 right안에서만 나오는값이기때문에
+             //나중에 left가 right를 넘길 수 없음
         }
 
         return sb.toString();
