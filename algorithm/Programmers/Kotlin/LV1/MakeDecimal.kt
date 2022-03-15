9779859feb53add7839ac19e1fe87ce3f0789093
jundonghyun <j950912@naver.com>
2022-03-15 15:24:44 +0900

    프로그래머스 소수만들기 DFS버전

---
+++ algorithm/Programmers/Kotlin/LV1/MakeDecimal.kt
@@ -0,0 +1,69 @@
+
+
+public class MakeDecimal {
+    var result: Int = 0
+    var temp: Int = 0
+    fun main(){
+
+        var nums = intArrayOf(1,2,3,4)
+
+        var result = solution(nums)
+        println(result)
+    }
+
+    fun solution(nums: IntArray): Int {
+        var answer = -1
+
+        var visit = BooleanArray(nums.size){false}
+        var count = 0
+        var idx = 0
+
+        answer = dfs(nums, visit, count, idx)
+
+        return answer
+    }
+
+    fun dfs(nums: IntArray, visit: BooleanArray, c: Int, idx: Int): Int{
+        var count = c
+        var index = idx
+
+        if(count == 3){
+            if(isPrime(temp)){
+                result++
+            }
+            return -1
+        }
+
+        while(index != nums.size){
+            if(!visit[index]){ //방문하지 않았다면
+                visit[index] = true
+                count++
+                temp += nums[index]
+                index++
+                dfs(nums, visit, count, index)
+                count--;
+                index--
+                visit[index] = false
+                temp -= nums[index]
+                index++
+            }
+        }
+        return result
+    }
+
+    fun isPrime(num: Int): Boolean{
+        for (i in 2 until num) {
+            if (num % i === 0) return false
+        }
+        return true
+    }
+
+}
+
+fun main(){
+    var t = MakeDecimal()
+
+    t.main()
+}
+
+
